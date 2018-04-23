d6c28cd99aa3428c9a0a3810fe12eb35ed350c0e
liany <601292628@qq.com>
2018-04-23 16:26:57 +0800

    0

--- src/SimpleAnimation.java
+++ src/SimpleAnimation.java
@@ -33,10 +33,10 @@ public class SimpleAnimation {
         }
     }
 
-    private class MyDrawPanel extends JPanel {
+    public class MyDrawPanel extends JPanel {   //这是一个内部类
         public void paintComponent(Graphics g) {
             g.setColor(Color.white);//每次画圆圈前将背景用白色填充，避免留下上一次画圆的重影
-            g.fillRect(0,0,this.getWidth(),this.getHeight());
+            g.fillRect(0,0,this.getWidth(),this.getHeight()); // 内部类可以调用外部类的参数
 
             g.setColor(Color.orange);
             g.fillOval(x, y, 40, 40);
